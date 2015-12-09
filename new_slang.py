5e7636390b903c2296678c72ae6ad184a714c83c
Stephen <lonelytreesl@yahoo.com>
2015-12-09 12:37:50 -0800

    translate multiple words properly.

--- new_slang.py
+++ new_slang.py
@@ -2,20 +2,26 @@ fileName = "translations.csv"
 
 READ = "r"
 
-userText =  raw_input("What is the text message you would like to translate?").lower()
+userText = raw_input("What is the text message you would like to translate?").lower()
 
 userWords = userText.split()
 
 translatedWords = []
 
-with open(fileName, mode=READ) as translationFile:
+for word in userWords:
+    counter = 0
+    wordTranslated = False
 
-    # for row in translationFile:
-    for word in userWords:
+    with open(fileName, mode=READ) as translationFile:
         for row in translationFile:
             wordsInRow = row.split(",")
+
             if word == wordsInRow[0].lower():
-                translatedWords.append(wordsInRow[1].lower())
+                translatedWords.append(wordsInRow[1].rstrip("\n").lower())
+                wordTranslated = wordTranslated = True
+                counter = counter + 1
 
+        if wordTranslated == False and counter == 0:
+            translatedWords.append(word)
 
-print(translatedWords)
+print(" ".join(translatedWords))
