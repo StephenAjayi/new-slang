a1445eb7e2a2ff940d621154ea5dcef9dd0648d0
Stephen <lonelytreesl@yahoo.com>
2015-12-08 17:54:37 -0800

    translate a single word properly.

--- new_slang.py
+++ new_slang.py
@@ -1,3 +1,21 @@
+fileName = "translations.csv"
+
+READ = "r"
+
 userText =  raw_input("What is the text message you would like to translate?").lower()
 
-words = userText.split()
+userWords = userText.split()
+
+translatedWords = []
+
+with open(fileName, mode=READ) as translationFile:
+
+    # for row in translationFile:
+    for word in userWords:
+        for row in translationFile:
+            wordsInRow = row.split(",")
+            if word == wordsInRow[0].lower():
+                translatedWords.append(wordsInRow[1].lower())
+
+
+print(translatedWords)
